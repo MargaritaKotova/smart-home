593442793e6454095bb203427af67bbf99afb5cb
MargaritaKotova <ritak2404@gmail.com>
2017-12-13 12:39:38 +0300

    modify

--- src/main/java/ru/sbt/mipt/oop/LightEventProcessor.java
+++ src/main/java/ru/sbt/mipt/oop/LightEventProcessor.java
@@ -1,29 +1,38 @@
 package ru.sbt.mipt.oop;
 
 
-import static ru.sbt.mipt.oop.SensorEventType.LIGHT_OFF;
-import static ru.sbt.mipt.oop.SensorEventType.LIGHT_ON;
+import static ru.sbt.mipt.oop.SensorEventType.*;
 
 public class LightEventProcessor implements EventHandler {
 
     @Override
     public void handle(SmartHome smartHome, SensorEvent event) {
-        if (event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF) {
+        if (!LightEvent(event))
+            return;
+
             // событие от источника света
             for (Room room : smartHome.getRooms()) {
                 for (Light light : room.getLights()) {
                     if (light.getId().equals(event.getObjectId())) {
-                        if (event.getType() == LIGHT_ON) {
-                            light.setOn(true);
-                            System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
-                        } else {
-                            light.setOn(false);
-                            System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
-                        }
+                        StatusOfLight(event, room, light);
                     }
                 }
             }
+
+    }
+
+    public void StatusOfLight(SensorEvent event, Room room, Light light) {
+        if (event.getType() == LIGHT_ON) {
+            light.setOn(true);
+            System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
+        } else {
+            light.setOn(false);
+            System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
         }
+    }
+
+    private boolean LightEvent(SensorEvent event) {
+        return  (event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF);
     }
 
 }
