9ca0a58f90359534007b9ac6135cb8ee23e13263
MargaritaKotova <ritak2404@gmail.com>
2017-12-13 23:04:13 +0300

    test 1.01 composite

---
+++ Test/ru/sbt/mipt/oop/TestCompoosite.java
@@ -0,0 +1,43 @@
+package ru.sbt.mipt.oop;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class TestCompoosite {
+    @Test
+    public void testActionableComposite() {
+        List<Light> lights1 = Arrays.asList(new Light("1", false), new Light("2", true));
+        List<Door> doors1 = Arrays.asList(new Door(false, "1"));
+        Room kitchen = new Room(lights1, doors1,"kitchen");
+
+        List<Light> lights2 = Arrays.asList(new Light("3", true));
+        List<Door> doors2 = Arrays.asList(new Door(false, "2"));
+        Room bathroom = new Room(lights2, doors2,"bathroom");
+
+        SmartHome smartHome = new SmartHome(Arrays.asList(kitchen, bathroom));
+
+        Set<Object> unvisitedObjects = new HashSet<>();
+        unvisitedObjects.add(smartHome);
+        unvisitedObjects.add(kitchen);
+        unvisitedObjects.add(bathroom);
+        unvisitedObjects.addAll(lights1);
+        unvisitedObjects.addAll(lights2);
+        unvisitedObjects.addAll(doors1);
+        unvisitedObjects.addAll(doors2);
+
+        smartHome.executeAction(unvisitedObjects::remove);
+        smartHome.executeAction(object -> {
+            unvisitedObjects.remove(object);
+        });
+        assertTrue(unvisitedObjects.isEmpty());
+
+    }
+}
